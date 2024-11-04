c4ba98a3b7b36675e5cb53d1c58d954103256d49
Darshan Gummadi <darshangummadi@usf.edu>
2024-11-04 00:55:59 -0600

    react-episode6-a

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,37 @@
 import ResCard from "./ResCard.js"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import resList from "../utils/mockData.js";
+import Shimmer from "./Shimmer.js";
 
 const Body = () => {
     //React state variable - super powerful variable
     //const [listOfRestaurants] = useState([]);
     //normal JS Variable
-    let [listOfRestaurants, setListOfRestaturant] = useState(resList); 
+    let [listOfRestaurants, setListOfRestaturant] = useState([]);
+
+    
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    const fetchData = async() => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.6287557&lng=79.4191795&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        console.log(json);
+
+        //not a good way to write this type and we can use option chaining
+
+        setListOfRestaturant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    //this is also not a good way
+    //use shimmer UI
+    if(listOfRestaurants.length === 0){
+        return <Shimmer/>;
+    }
+
     return (
         <div className="body">
             <div className="filter">
